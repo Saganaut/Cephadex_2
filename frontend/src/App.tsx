681e37370d1f36109110305bab56c4332204a3e2
Root99 <azizrezgui4@gmail.com>
2023-10-31 16:25:21 +0100

    added types folder, for global types.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import "./styles/App.css";
 import "./styles/output.css";
 
 import { ContextWrapper } from "@contexts/ContextWrapper";
-import React from "react";
+import React, { type ReactElement } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { AppRoutes } from "./routes/AppRoutes";
 
-const App = () => {
+const App = (): ReactElement => {
   return (
     <ContextWrapper>
       <Router>
