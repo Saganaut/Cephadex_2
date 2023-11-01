7ab3304b41d2d6db87de5a19f8ac2ab7fa376b3a
Root99 <azizrezgui4@gmail.com>
2023-11-01 17:56:34 +0100

    Added Account Form & Validation & InputField & Dropdown

---
+++ frontend/src/components/Pages/Account/InputField.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+interface InputFieldProps {
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+  placeholder: string;
+  label?: string;
+}
+const InputField: React.FC<InputFieldProps> = ({
+  name,
+  value,
+  onChange,
+  onBlur,
+  type,
+  placeholder,
+  label,
+}) => {
+  return (
+    <div>
+      <p className={"pb-2 text-[20px] font-medium text-white"}>{label}</p>
+      <input
+        autoComplete={"off"}
+        className="w-full max-w-[650px] appearance-none rounded-[18px] border bg-transparent px-[18px]  py-[20px] text-[18px] leading-tight text-aquamarine shadow placeholder:text-aquamarine/30 focus:outline-none"
+        type={type}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        onChange={onChange}
+        value={value}
+        name={name}
+      />
+    </div>
+  );
+};
+
+interface TextAreaFieldProps {
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onBlur: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  placeholder: string;
+  label?: string;
+}
+const TextAreaField: React.FC<TextAreaFieldProps> = ({
+  name,
+  onBlur,
+  onChange,
+  value,
+
+  placeholder,
+}) => {
+  return (
+    <div>
+      <textarea
+        className="focus:shadow-outline w-full appearance-none rounded border bg-mariana-blue px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+        placeholder={placeholder}
+        rows={4}
+        onBlur={onBlur}
+        onChange={onChange}
+        value={value}
+        name={name}
+      ></textarea>
+    </div>
+  );
+};
+
+export { InputField };
+export { TextAreaField };
