95bf0caab40856057e9f90ff38333d6ada114f40
Root99 <azizrezgui4@gmail.com>
2023-11-02 14:21:40 +0100

    revert.

--- frontend/src/services/Auth.tsx
+++ frontend/src/services/Auth.tsx
@@ -1,27 +1,20 @@
-import { type CredentialResponse } from "@react-oauth/google";
-import { axiosPrivate } from "@services/axios";
-import { type AxiosResponse } from "axios";
+import axios from "axios";
 
-const sendGoogleSignInToken = async (
-  credentialResponse: CredentialResponse
-): Promise<AxiosResponse<any, any>> => {
+const sendGoogleSignInToken = async (credentialResponse) => {
   try {
-    const response = await axiosPrivate.post(
-      "/user_bp/api_0/auth/google-sign-in",
+    const response = await axios.post(
+      "http://localhost:5000/user_bp/api_0/auth/google-sign-in",
       credentialResponse,
       { headers: { "Content-Type": "application/json" } }
     );
 
     if (response.data.status === "success") {
       console.log("Login successful:", response.data);
-      return response;
     } else {
       console.error("Login failed:", response.data.error);
-      return response;
     }
   } catch (error) {
     console.error("An error occurred while sending token to backend:", error);
-    return "error" as any;
   }
 };
 
