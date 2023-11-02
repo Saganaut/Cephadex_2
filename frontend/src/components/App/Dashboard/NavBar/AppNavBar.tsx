279e2fb3652e3f89eb360189ff49757f6b535146
Root99 <azizrezgui4@gmail.com>
2023-11-02 13:21:10 +0100

    Updated the Navbar.

--- frontend/src/components/App/Dashboard/NavBar/AppNavBar.tsx
+++ frontend/src/components/App/Dashboard/NavBar/AppNavBar.tsx
@@ -1,20 +1,44 @@
 import { NavBarButtonCreate } from "@app/Dashboard/NavBar/NavBarButtons";
-import { SelectionButtonContainer } from "@app/Dashboard/NavBar/SelectionButtonContainer";
 import { NavBarUpgradeLink } from "@app/Dashboard/SideBar/SideBarLink";
 import { ThemeToggle } from "@app/ThemeToggle";
+import Avatar from "@assets/UserAvatar.svg";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 const NavBarApp: React.FC = () => {
   return (
-    <header className="absolute left-1/2 right-0 top-0 mt-4 flex -translate-x-1/2 items-center  rounded-bl-3xl p-2 text-white">
-      <nav>
-        <div className="flex flex-row items-center">
+    <header className="relative left-1/2 right-0 top-0 mt-[64px] w-full -translate-x-1/2 items-center  rounded-bl-3xl">
+      <div
+        className={
+          "ml-auto flex w-full max-w-[1250px] justify-between px-[60px]"
+        }
+      >
+        {/*   LEFT  */}
+        <div className={"flex items-center gap-x-[54px]"}>
+          <button
+            className={
+              "flex items-center gap-x-[10px] rounded-full bg-electric-violet p-[8px]"
+            }
+          >
+            <div
+              className={
+                "flex h-[42px] w-[42px] items-center justify-center rounded-full bg-gray-200"
+              }
+            >
+              <ChevronDoubleLeftIcon className={"h-[24px] w-[24px]"} />
+            </div>
+            <span className={"pr-4 text-white"}>Go Back</span>
+          </button>
+
           <NavBarButtonCreate />
-          <SelectionButtonContainer />
+        </div>
+        {/*   RIGHT */}
+        <div className={"flex items-center gap-x-[54px]"}>
           <NavBarUpgradeLink />
           <ThemeToggle />
+          <img src={Avatar} alt="avatar-img" className={"h-[44px] w-[44px]"} />
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
