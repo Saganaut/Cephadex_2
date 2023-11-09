726c463b0c1aef54518370817ac09aaa455993fb
Root99 <azizrezgui4@gmail.com>
2023-11-09 23:14:10 +0100

    added increment & decrement for MCQ cards & styling for correct/wrong answers.

--- frontend/src/components/App/Features/Deck/FlashCard.tsx
+++ frontend/src/components/App/Features/Deck/FlashCard.tsx
@@ -1,3 +1,4 @@
+import { decrementCard, incrementCard } from "@app/Features/Study/Study";
 import { CardStructure } from "@source/components/App/Shared/CardStructure";
 import { type Card } from "@source/types/Deck";
 import React, { useEffect, useState } from "react";
@@ -13,10 +14,23 @@ const shuffleMCQ = (array: Array<string | null>): Array<string | null> => {
   }
   return shuffledArray;
 };
+
 const FlashCard: React.FC<FlashCardProps> = ({ card }) => {
   const [shuffledValues, setShuffledValues] = useState<Array<string | null>>(
     []
   );
+  const [answer, setAnswer] = useState<string | null>(null);
+
+  const handleCardUpdate = async (answer: string | null): Promise<void> => {
+    setAnswer(answer);
+    const isCorrect = answer === card.content;
+    if (isCorrect) {
+      await decrementCard(card.id);
+    }
+    if (!isCorrect) {
+      await incrementCard(card.id);
+    }
+  };
 
   useEffect(() => {
     // Extract values from the card object
@@ -44,22 +58,74 @@ const FlashCard: React.FC<FlashCardProps> = ({ card }) => {
           {card.category === "Mcq" && (
             <>
               <ul>
-                <li>A - {shuffledValues[0]}</li>
-                <li>B - {shuffledValues[1]}</li>
-                <li>C - {shuffledValues[2]}</li>
-                <li>D - {shuffledValues[3]}</li>
+                <li
+                  className={`${
+                    answer != null && card.content === shuffledValues[0]
+                      ? "text-green-500"
+                      : answer != null && card.content !== shuffledValues[0]
+                      ? "text-red-700"
+                      : "text-white"
+                  }`}
+                  onClick={async () => {
+                    await handleCardUpdate(shuffledValues[0]);
+                  }}
+                >
+                  A - {shuffledValues[0]}
+                </li>
+                <li
+                  className={`${
+                    answer != null && card.content === shuffledValues[1]
+                      ? "text-green-500"
+                      : answer != null && card.content !== shuffledValues[1]
+                      ? "text-red-700"
+                      : "text-white"
+                  }`}
+                  onClick={async () => {
+                    await handleCardUpdate(shuffledValues[1]);
+                  }}
+                >
+                  B - {shuffledValues[1]}
+                </li>
+                <li
+                  className={`${
+                    answer != null && card.content === shuffledValues[2]
+                      ? "text-green-500"
+                      : answer != null && card.content !== shuffledValues[2]
+                      ? "text-red-700"
+                      : "text-white"
+                  }`}
+                  onClick={async () => {
+                    await handleCardUpdate(shuffledValues[2]);
+                  }}
+                >
+                  C - {shuffledValues[2]}
+                </li>
+                <li
+                  className={`${
+                    answer != null && card.content === shuffledValues[3]
+                      ? "text-green-500"
+                      : answer != null && card.content !== shuffledValues[3]
+                      ? "text-red-700"
+                      : "text-white"
+                  }`}
+                  onClick={async () => {
+                    await handleCardUpdate(shuffledValues[3]);
+                  }}
+                >
+                  D - {shuffledValues[3]}
+                </li>
               </ul>
             </>
           )}
         </div>
         <div className="card-stats mt-3">
           Data:
-          <p> created on - {card["time-created"]}</p>
-          <p> accessed on- {card["time-updated"]}</p>
-          <p> times correct - {card["times-correct"]}</p>
-          <p> times asked - {card["times-asked"]}</p>
-          <p> difficulty level - {card["diff-lvl"]}</p>
-          <p> Next time it will be asked - {card["srs-interval"]}</p>
+          <p> created on : {card["time-created"]}</p>
+          <p> accessed on : {card["time-updated"]}</p>
+          <p> times correct : {card["times-correct"]}</p>
+          <p> times asked : {card["times-asked"]}</p>
+          <p> difficulty level : {card["diff-lvl"]}</p>
+          <p> Next time it will be asked : {card["srs-interval"]}</p>
         </div>
       </CardStructure>
     </div>
